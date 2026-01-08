c4a1da48319274795ec5b13689a92a54dede350f
skalka2000 <piotrpawel.skala@gmail.com>
2026-01-08 23:52:07 +0100

    New color

--- src/utils/ratingColors.js
+++ src/utils/ratingColors.js
@@ -1,7 +1,8 @@
 export function getRatingColor(rating) {
   if (rating === 11) return "rating-circle rating-gold";
-  if (rating <= 10 && rating >= 8.5) return "rating-circle rating-dark-green";
-  if (rating < 8.5 && rating >= 7.0) return "rating-circle rating-light-green";
+  if (rating <= 10 && rating >= 9.0) return "rating-circle rating-dark-green";
+  if (rating < 9.0 && rating >= 8.0) return "rating-circle rating-green"
+  if (rating < 8.0 && rating >= 7.0) return "rating-circle rating-light-green";
   if (rating < 7.0 && rating >= 5.0) return "rating-circle rating-orange";
   return "rating-circle rating-red";
 }
